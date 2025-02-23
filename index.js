244eb609eb03567a9d135491119a0a4532b5e7b8
BernCell <bern.cell@hotmail.com>
2025-02-24 00:09:51 +0100

    1rst commit acadee2025-Ts

---
+++ index.js
@@ -0,0 +1,4 @@
+"use strict";
+let message = "Hello World !!";
+let age = 20;
+console.log(message + ", j'ai " + age + " ans.");
---
+++ index.ts
@@ -0,0 +1,5 @@
+let message: string = "Hello World !!"
+let age: string | number =  20
+
+console.log(message + " j'ai " + age + " ans.");
+
